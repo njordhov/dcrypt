6849e517c819c9b3dd064b349ae32d8e3348c317
Terje Norderhaug <terje@in-progress.com>
2020-02-26 18:25:00 -0800

    Update privacy policy

--- src/components/config.js
+++ src/components/config.js
@@ -3,6 +3,7 @@ export const app = process.env.REACT_APP_TAG
 export const title = process.env.REACT_APP_TITLE
 const message = (app == "message-app") || (app == "dcrypt-app")
 const files = (app == "drop-app") || (app == "dcrypt-app")
+const privacyUrl = process.env.REACT_APP_PRIVACY_URL
 export const features = {message: message, files: files}
-const config = {app, features, title}
+const config = {app, features, title, privacyUrl}
 export default config
